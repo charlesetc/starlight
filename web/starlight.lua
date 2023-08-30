4e60de3107348e43ab78e110c88186c2f018a4ad
Charles Chamberlain <charles@nrwhl.xyz>
2023-08-30 13:45:14 +0200

    Slightly better web

--- web/starlight.lua
+++ web/starlight.lua
@@ -1,9 +1,19 @@
-Write([[<!DOCTYPE>]])
-Write([[<head>]])
-Write([[<title>starlight</title>]])
-Write([[<script src="/js/framework.js">]])
-Write([[</script>]])
-Write([[</head>]])
-Write([[<body>]])
-Write([[<h1>hi there</h1>]])
-Write([[</body>]])
+function WriteBody()
+  Write([[<h1>hi there you!</h1>]])
+end
+
+function WriteHTML()
+  SetStatus(200)
+  SetHeader('Content-Type', 'text/html; charset=utf-8')
+  Write([[<!DOCTYPE>]])
+  Write([[<head>]])
+  Write([[<title>starlight</title>]])
+  Write([[<script src="/js/framework.js">]])
+  Write([[</script>]])
+  Write([[</head>]])
+  Write([[<body>]])
+  WriteBody()
+  Write([[</body>]])
+end
+
+WriteHTML()
